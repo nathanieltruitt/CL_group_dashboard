fce1f65003c905f0b09d23bd62c20b4c01bdfc7e
LanceScheffer <lancescheffer@gmail.com>
2022-11-21 14:21:52 -0600

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InfoBoxComponent } from './info-box/info-box.component';
+import { InfoBoxesComponent } from './info-boxes/info-boxes.component';
+import { DynamicBoxComponent } from './dynamic-box/dynamic-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InfoBoxComponent,
+    InfoBoxesComponent,
+    DynamicBoxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dynamic-box/dynamic-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-box',
+  templateUrl: './dynamic-box.component.html',
+  styleUrls: ['./dynamic-box.component.css']
+})
+export class DynamicBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
