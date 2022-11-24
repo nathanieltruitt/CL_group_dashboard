7aebdfd6a114ee0de2eba0730f050192b03bfad6
LanceScheffer <lancescheffer@gmail.com>
2022-11-24 12:08:18 -0600

    This is incomplete.
    Started working on this yesterday. I doubt I have time to make it look right. Just figured I needed the practice.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { InfoBoxComponent } from './info-box/info-box.component';
 import { InfoBoxesComponent } from './info-boxes/info-boxes.component';
 import { DynamicBoxComponent } from './dynamic-box/dynamic-box.component';
 
@@ -11,7 +10,6 @@ import { DynamicBoxComponent } from './dynamic-box/dynamic-box.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    InfoBoxComponent,
     InfoBoxesComponent,
     DynamicBoxComponent
   ],
